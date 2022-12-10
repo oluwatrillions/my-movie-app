9b0587019df89e202fb1bb682f359d9cc6db3063
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-12-10 11:10:00 +0100

    firct commit-movie app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Movies from './Movies';
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    const movie_api = 'https://www.omdbapi.com/?s=star%20wars&apikey=98cbfa52'
+
+          const [movies, setMovies] = useState([])
+        try {
+                useEffect(() => {
+                axios(movie_api)
+                    .then((response) => {
+                        console.log(response.data.Search);
+                        setMovies(response.data.Search)
+                    })
+            }, [])
+        } catch (error) {
+            console.log(error);
+        }
+        return (
+        <div className="App">
+         <h1>{movies.map((movie) => {
+            const {Poster,Title, Year, imdbID} = movie
+            return <div key={imdbID}>
+                {Title}
+                {Year}
+                <img src={Poster} alt={Title} />
+                </div>
+        }) }</h1>
     </div>
   );
 }
