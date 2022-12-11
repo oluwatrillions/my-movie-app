1bc76868a01fa70bcd263f37d75fc9daede90a9c
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-12-12 00:02:38 +0100

    search params

--- src/App.js
+++ src/App.js
@@ -4,30 +4,51 @@ import Movies from './Movies';
 import axios from 'axios'
 
 function App() {
+    const API_KEY = '98cbfa52'
+    const movie_api = `https://www.omdbapi.com/?s=avengers&apikey=${API_KEY}`
+    
 
-    const movie_api = 'https://www.omdbapi.com/?s=star%20wars&apikey=98cbfa52'
+        const [search, setSearch] = useState('')
+        const [movies, setMovies] = useState([])
 
-          const [movies, setMovies] = useState([])
-        try {
-                useEffect(() => {
+            useEffect(() => {
                 axios(movie_api)
                     .then((response) => {
-                        console.log(response.data.Search);
+                        console.log(response.data);
                         setMovies(response.data.Search)
                     })
-            }, [])
-        } catch (error) {
-            console.log(error);
-        }
+            }, [search])
+
+    const searchBtn = async (title) => {
+        const searchURL = `https://www.omdbapi.com/?s=${search}&apikey=${API_KEY}`
+        console.log(search);
+        console.log(API_KEY);
+
+        const info = await axios(searchURL)
+                        .then(response => response.json())
+                            .then(data => setMovies(data.Search))
+                                .catch(error => console.log(error))
+                    
+}
     
-        return (
-            <div className="App">
-                
+    return (
+        <>
+            <div className='search'>
+                <h2>Movie App</h2>
+                <div className='search-box'>
+                    <form type='text'>
+                        <input type='text' value={search} onChange={((e) => setSearch(e.target.value))} />
+                        <button onClick={() => searchBtn(search)}>Search</button>
+                    </form>
+                </div>
+            </div>
+            <div className="App">           
             {movies.map((movie) => (
                 <Movies key={ movie.imdbID} {...movie} />
             ))
         }
     </div>
+            </>
   );
 }
 
--- src/Movies.js
+++ src/Movies.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './Movies.css'
 
 
-const Movies = ({Title, Year, Poster}) => {
+const Movies = ({Title, Year, Poster, imdbID}) => {
     return (
-        <div className='movie-div'>
-            <img src={Poster} alt={Title} />
+      <div className='movie-container'>
+        <div className='movie-div' key={imdbID}>
+            <img src={Poster} alt={Title} className='image'/>
             <h2>{Title}</h2>
             <h5>{ Year}</h5>
         </div>
+    </div> 
   )
 }
 
